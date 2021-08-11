2cc76a464c75fdc4647c0ad8c249d8c48fb921f8
farreldarian <farreldarian@icloud.com>
2021-08-11 12:09:12 +0700

    Fix default provider bug

--- src/actions/parse-args/index.ts
+++ src/actions/parse-args/index.ts
@@ -3,7 +3,7 @@ import parseProvider from './parse-provider'
 function parseArgs(args: any) {
   const provider = parseProvider(args.provider)
 
-  return { provider, ...args }
+  return { ...args, provider }
 }
 
 export default parseArgs
