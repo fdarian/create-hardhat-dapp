4cfd677901eccf015438935e557a1e1841580a6d
farreldarian <farreldarian@icloud.com>
2021-08-11 10:55:27 +0700

    Add chain constants

---
+++ src/constants/chains.ts
@@ -0,0 +1,23 @@
+export enum ChainId {
+  Mainnet = 1,
+  Ropsten = 3,
+  Rinkeby = 4,
+  Goerli = 5,
+  Kovan = 42,
+  BSC = 56,
+  xDai = 100,
+  Polygon = 137,
+  Mumbai = 80001,
+}
+
+export const CHAIN_NAMES = {
+  [ChainId.Mainnet]: 'Mainnet',
+  [ChainId.Ropsten]: 'Ropsten',
+  [ChainId.Kovan]: 'Kovan',
+  [ChainId.Rinkeby]: 'Rinkeby',
+  [ChainId.Goerli]: 'Goerli',
+  [ChainId.BSC]: 'BSC',
+  [ChainId.xDai]: 'xDai',
+  [ChainId.Polygon]: 'Polygon',
+  [ChainId.Mumbai]: 'Mumbai',
+}
